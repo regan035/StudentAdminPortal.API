ca7eed5c466717a9b2c179dc031253a56441b1e5
gen <regan035@gmail.com>
2022-12-14 00:12:14 -0600

    Add project files.

---
+++ StudentAdminPortal.API/Data/AppDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentAdminPortal.API.DataModels;
+
+namespace StudentAdminPortal.API.Data
+{
+    public class AppDBContext:DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext>options):base(options) 
+        {
+        }
+        public DbSet<Student> Student { get; set; }
+        public DbSet<Gender> Gender { get; set; }
+        public DbSet<Address> Address { get; set; }
+
+    }
+}
